68538d70582d595bec120939ebe704f1e506ec48
STEPIN104305 <67002283+STEPIN104305@users.noreply.github.com>
2020-10-07 19:29:49 +0530

    Update project_main.c

--- MiniProject_C/3_Implementation/project_main.c
+++ MiniProject_C/3_Implementation/project_main.c
@@ -1,112 +1,157 @@
-#include <calculator_operations.h>
+#include <stdio.h>
+#include <time.h>
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+/*check given year is leap year or not*/
+int isLeapYear(int year, int mon)
 {
-    printf("\n****Calculator****\n");
-    while(1)
+    int flag = 0;
+    if (year % 100 == 0)
     {
-        calculator_menu();
+            if (year % 400 == 0)
+            {
+                    if (mon == 2)
+                    {
+                            flag = 1;
+                    }
+            }
     }
+    else if (year % 4 == 0)
+    {
+            if (mon == 2)
+            {
+                    flag = 1;
+            }
+    }
+    return (flag);
 }
 
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     __fpurge(stdin);
-    scanf("%d", &calculator_operation);
 
-    if(EXIT == calculator_operation)
+int main()
+{
+
+    int DaysInMon[] = {31, 28, 31, 30, 31, 30,
+                       31, 31, 30, 31, 30, 31};
+    int days, month, year;
+    char dob[100];
+    time_t ts;
+    struct tm *ct;
+
+    /* enter date of birth */
+    printf("Enter your date of birth (DD/MM/YYYY): ");
+    scanf("%d/%d/%d",&days,&month, &year);
+
+    /*get current date.*/
+    ts = time(NULL);
+    ct = localtime(&ts);
+
+    printf("Current Date: %d/%d/%d\n",
+            ct->tm_mday, ct->tm_mon + 1, ct->tm_year + 1900);
+
+    days = DaysInMon[month - 1] - days + 1;
+
+    /* leap year checking*/
+    if (isLeapYear(year, month))
     {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
+            days = days + 1;
     }
 
-    if(INVALID != valid_operation(calculator_operation))
+    /* calculating age in no of days, years and months */
+    days = days + ct->tm_mday;
+    month = (12 - month) + (ct->tm_mon);
+    year = (ct->tm_year + 1900) - year - 1;
+
+    /* checking for leap year feb - 29 days */
+    if (isLeapYear((ct->tm_year + 1900), (ct->tm_mon + 1)))
     {
-        printf("\n\tEnter your Numbers with space between them\n");
-        __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
+            if (days >= (DaysInMon[ct->tm_mon] + 1))
+            {
+                    days = days - (DaysInMon[ct->tm_mon] + 1);
+                    month = month + 1;
+            }
+    }
+    else if (days >= DaysInMon[ct->tm_mon])
+    {
+            days = days - (DaysInMon[ct->tm_mon]);
+            month = month + 1;
+    }
+
+    if (month >= 12)
+    {
+            year = year + 1;
+            month = month - 12;
+    }
+
+    /* print age */
+    printf("\n## Hey you are  %d years %d months and %d days old. ##\n", year, month, days);
+    /* print life period */
+    printf("The Life Period based on the generated age is : ");
+    if(year>=0 && year<=11)
+    {
+        printf("Chidhood");
+        if(year==0 && days<=42 && month<=1)
+        {
+           printf("(Newborn Child)");
+        }
+        else if(year==1 && days>42 && month>=1)
+        {
+            printf("(Suckling)");
+        }
+        else if(year>=1 && year<=3)
+        {
+            printf("(Early Childhood)");
+        }
+        else if(year>=4 && year<=7)
+        {
+            printf("(Pre-school Childhood)");
+        }
+        else
+        {
+            printf("(School Childhood)");
+        }
+    }
+    else if(year>=12 && year<=20)
+    {
+        printf("Adolescence");
+        if(year>=12 && year<=15)
+        {
+            printf("(Puberty)");
+        }
+        else
+        {
+            printf("(Adolescence)");
+        }
+    }
+    else if(year>=21 && year<=60)
+    {
+        printf("Adulthood");
+        if(year>=21 && year<=30)
+        {
+            printf("(Early Adulthood)");
+        }
+        else if(year>=31 && year<=45)
+        {
+            printf("(Middle Adulthood)");
+        }
+        else
+        {
+            printf("(Late Adulthood)");
+        }
     }
     else
     {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        __fpurge(stdin);
-        getchar();
-        return;
-        
+        printf("Old Age");
+        if(year>=61 && year<=75)
+        {
+            printf("(Early Old Age)");
+        }
+        else if(year>=76 && year<=90)
+        {
+            printf("(Middle Old Age)");
+        }
+        else
+        {
+            printf("(Late Old Age)");
+        }
     }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
-}
-
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+    return 0;
 }
